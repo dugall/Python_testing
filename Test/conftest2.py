17561b2985c25e204194f75609ca7468924e1877
Vladislav Kurdyukov <funray.pro@gmail.com>
2023-12-06 13:13:29 -0500

    PyTest practice

---
+++ Test/conftest2.py
@@ -0,0 +1,11 @@
+# content of conftest.py
+import pytest
+import smtplib
+
+
+@pytest.fixture(scope="module", params=["smtp.gmail.com", "mail.python.org"])
+def smtp_connection(request):
+    smtp_connection = smtplib.SMTP(request.param, 587, timeout=5)
+    yield smtp_connection
+    print("finalizing {}".format(smtp_connection))
+    smtp_connection.close()
---
+++ Test/test_maybe_palindrome.py
@@ -0,0 +1,16 @@
+import pytest
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+@pytest.mark.parametrize("maybe_palindrome, expected_result", [
+    ("", True),
+    ("a", True),
+    ("Bob", True),
+    ("Never odd or even", True),
+    ("Do geese see God?", True),
+    ("abc", False),
+    ("abab", False),
+])
+def test_is_palindrome(maybe_palindrome, expected_result):
+    assert is_palindrome(maybe_palindrome) == expected_result
